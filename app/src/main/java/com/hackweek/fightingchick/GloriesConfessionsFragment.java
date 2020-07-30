8e9a2dea8324318cc64d29ad261f13b13c00bd99
zhusiyi <xiaosiyier@163.com>
2020-07-30 21:15:02 +0800

    记录部分的Tablayout

---
+++ app/src/main/java/com/hackweek/fightingchick/GloriesConfessionsFragment.java
@@ -0,0 +1,31 @@
+package com.hackweek.fightingchick;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GloriesConfessionsFragment extends Fragment {
+
+
+    public GloriesConfessionsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_glories_confessions, container, false);
+    }
+}
