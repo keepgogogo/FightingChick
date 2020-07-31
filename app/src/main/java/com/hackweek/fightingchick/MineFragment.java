c6ef0810c124f3ee0c42a99789532e9bcb00c6ea
keepgogogo <57135073+keepgogogo@users.noreply.github.com>
2020-07-31 21:13:01 +0800

    修改个人资料fragment

--- app/src/main/java/com/hackweek/fightingchick/MineFragment.java
+++ app/src/main/java/com/hackweek/fightingchick/MineFragment.java
@@ -10,11 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
 
-    private Button aboutUs;
+    TextView nickNameTextView;
+    TextView fightingForeverSloganTextView;
+    Button buttonForSetPersonalStatics;
+    Button buttonForWatchGloryAndConfessions;
+    Button buttonForWatchAboutUs;
 
     public MineFragment() {
         // Required empty public constructor
@@ -30,19 +35,51 @@ public class MineFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+
+        nickNameTextView=(TextView)view.findViewById(R.id.NickName);
+        fightingForeverSloganTextView=(TextView)view.findViewById(R.id.FightingForeverSlogan);
+        buttonForSetPersonalStatics=(Button)view.findViewById(R.id.ButtonForChangePersonalDocument);
+        buttonForWatchAboutUs=(Button)view.findViewById(R.id.ButtonForWatchAboutUsInMineFragment);
+        buttonForWatchGloryAndConfessions=(Button)view.findViewById(R.id.ButtonForWatchGloryAndConfessionInMineFragment);
+
+
+
+        return view;
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        aboutUs = (Button)view.findViewById(R.id.ButtonForWatchAboutUsInMineFragment);
-        aboutUs.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                MainActivity activity = (MainActivity)getActivity();
-                Fragment fragment = new TodoListFragment();
-                activity.setFragment(fragment);
-            }
-        });
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.ButtonForChangePersonalDocument:
+                //编辑个人资料点击事件
+                MainActivity mainActivity=new MainActivity();
+                mainActivity.setFragment(new EditPersonalDocumentFragment());
+                break;
+            case R.id.ButtonForWatchAboutUsInMineFragment:
+                //关于我们点击事件
+
+                break;
+            case R.id.ButtonForWatchGloryAndConfessionInMineFragment:
+                //查看光荣录和忏悔录点击事件
+
+                break;
+            default:
+                break;
+        }
     }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        aboutUs = (Button)view.findViewById(R.id.ButtonForWatchAboutUsInMineFragment);
+//        aboutUs.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                MainActivity activity = (MainActivity)getActivity();
+//                Fragment fragment = new TodoListFragment();
+//                activity.setFragment(fragment);
+//            }
+//        });
+//    }
 }
