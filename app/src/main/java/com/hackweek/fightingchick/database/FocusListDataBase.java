e29d0e6b2a0a2fe03c83f9608ecb4ff0364a8c24
zhusiyi <xiaosiyier@163.com>
2020-08-02 17:49:14 +0800

    通知写了一点点

--- app/src/main/java/com/hackweek/fightingchick/database/FocusListDataBase.java
+++ app/src/main/java/com/hackweek/fightingchick/database/FocusListDataBase.java
@@ -1,7 +1,16 @@
 package com.hackweek.fightingchick.database;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {FocusList.class},version = 1,exportSchema = false)
 public abstract class FocusListDataBase extends RoomDatabase {
