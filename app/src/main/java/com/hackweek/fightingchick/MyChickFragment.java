3918bc1feb38a71a6b2a3b67677403a91f297adb
zhusiyi <xiaosiyier@163.com>
2020-08-06 17:37:38 +0800

    但愿没有太大bugug

--- app/src/main/java/com/hackweek/fightingchick/MyChickFragment.java
+++ app/src/main/java/com/hackweek/fightingchick/MyChickFragment.java
@@ -21,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.beardedhen.androidbootstrap.BootstrapProgressBar;
 
@@ -28,6 +29,7 @@ import com.beardedhen.androidbootstrap.BootstrapProgressBar;
 public class MyChickFragment extends Fragment implements View.OnClickListener{
 
     private int energyValue;
+    private int energyNeedForUpgrade;
     private TextView myChickName;
     private BootstrapProgressBar upgradeProgressBar;
     private TextView energyValueView;
@@ -38,6 +40,7 @@ public class MyChickFragment extends Fragment implements View.OnClickListener{
     private ImageButton nextChick;
     private ImageButton lastChick;
     private int currentChick;//0~6
+    private int mychick;
     public MyChickFragment() {
         // Required empty public constructor
     }
@@ -98,39 +101,58 @@ public class MyChickFragment extends Fragment implements View.OnClickListener{
                 public void run() {
                     int x=0;
                     if(num>0)
-                        x=chicks[num].getLeft()-300;
+                        x=chicks[num].getLeft()-250;
                     scrollView.smoothScrollTo(x,0);
                 }
             });
         }
     }
-
-
+    private void hideLeft(){ lastChick.setVisibility(View.GONE);lastChick.setClickable(false);}
+    private void hideRight(){ nextChick.setVisibility(View.GONE);nextChick.setClickable(false);}
+    private void showLeft(){ lastChick.setVisibility(View.VISIBLE);lastChick.setClickable(true);}
+    private void showRight(){nextChick.setVisibility(View.VISIBLE);nextChick.setClickable(true);}
 
     // initialize the name, image and progress bar according to current energy value
     private void setMyChick(){
         if(energyValue<-50){//拖拉鸡
+            hideLeft();
             currentChick=0;
+            mychick=0;
+            energyNeedForUpgrade=-50-energyValue;
             if(energyValue>-100){
                 upgradeProgressBar.setProgress(calculateProgress(-100,-50));
             }else upgradeProgressBar.setProgress(0);
         }else if(energyValue<0){//落汤鸡
+            hideLeft();
             currentChick=1;
+            mychick=1;
+            energyNeedForUpgrade=-energyValue;
             upgradeProgressBar.setProgress(calculateProgress(-50,0));
         }else if(energyValue<20){//白切鸡
+            hideLeft();
             currentChick=2;
+            mychick=2;
+            energyNeedForUpgrade=20-energyValue;
             upgradeProgressBar.setProgress(calculateProgress(0,20));
         }else if(energyValue<50){//跑步鸡
             currentChick=3;
+            mychick=3;
+            energyNeedForUpgrade=50-energyValue;
             upgradeProgressBar.setProgress(calculateProgress(20,50));
         }else if(energyValue<80){//滑翔鸡
             currentChick=4;
+            mychick=4;
+            energyNeedForUpgrade=80-energyValue;
             upgradeProgressBar.setProgress(calculateProgress(50,80));
         }else if(energyValue<100){//战斗鸡
             currentChick=5;
+            mychick=5;
+            energyNeedForUpgrade=100-energyValue;
             upgradeProgressBar.setProgress(calculateProgress(80,100));
         }else{//永动鸡
+            hideRight();
             currentChick=6;
+            mychick=6;
         }
         scrollToChick(currentChick);
     }
@@ -139,12 +161,22 @@ public class MyChickFragment extends Fragment implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.my_chick_last_chick:
-                if(currentChick>0){
+                showRight();
+                if(currentChick==mychick+1){
+                    hideLeft();
+                    currentChick-=1;
+                    scrollToChick(currentChick);
+                }else{
                     currentChick-=1;
                     scrollToChick(currentChick);
                 }
                 break;
             case R.id.my_chick_next_chick:
+                if(currentChick==mychick){
+                    Toast.makeText(getContext(), "还差"+energyNeedForUpgrade+"动力值解锁", Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                showLeft();
                 if(currentChick<6){
                     currentChick+=1;
                     scrollToChick(currentChick);
